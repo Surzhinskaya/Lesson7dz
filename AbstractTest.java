51053e0acba64f1b91283301b5f3668aa2a5a801
Surzhinskaya <95657122+Surzhinskaya@users.noreply.github.com>
2022-05-24 00:57:41 +0300

    Add files via upload

---
+++ AbstractTest.java
@@ -0,0 +1,81 @@
+package Lesson6;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+
+class AbstractTest {
+
+
+    static EventFiringWebDriver eventDriver;
+
+
+    @BeforeAll
+    static void setDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        //options.addArguments("--headless");
+        options.addArguments("start-maximized");
+        options.setPageLoadTimeout(Duration.ofSeconds(10));
+
+        options.setPageLoadTimeout(Duration.ofSeconds(10));
+
+        eventDriver = new EventFiringWebDriver(new ChromeDriver(options));
+        eventDriver.register(new MyWebDriverEventListener());
+
+        eventDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @BeforeEach
+    void initMainPage() {
+        Assertions.assertDoesNotThrow(() -> getWebDriver().navigate().to("https://www.superapteka.ru"),
+                "Страница не доступна");
+        Assertions.assertTrue(true);
+
+    }
+
+    @BeforeEach
+    void getPage() {
+        eventDriver.get("https://www.superapteka.ru");
+        eventDriver.manage().deleteAllCookies();
+    }
+
+
+    @AfterAll
+    public static void exit() {
+
+
+        if (eventDriver != null) eventDriver.quit();
+    }
+
+    @AfterEach
+    public void checkBrowser() {
+        List<LogEntry> allLogRows = getWebDriver().manage().logs().get(LogType.BROWSER).getAll();
+        if (!allLogRows.isEmpty()) {
+
+            if (allLogRows.size() > 0) {
+                allLogRows.forEach(logEntry -> {
+                    System.out.println(logEntry.getMessage());
+                });
+
+            }
+
+        }
+
+    }
+
+    public WebDriver getWebDriver() {
+        return this.eventDriver;
+    }
+
+}
